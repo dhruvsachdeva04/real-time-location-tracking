496f930f6c20126f662f6e4cb8c9420d00eb19ee
dhruvsachdeva04 <dhruvsachdeva3828@gmail.com>
2024-11-12 00:46:05 +0530

    real time location added

---
+++ public/js/script.js
@@ -0,0 +1,43 @@
+const socket = io();
+
+if (navigator.geolocation) {
+  navigator.geolocation.watchPosition(
+    (position) => {
+      const { latitude, longitude } = position.coords;
+      socket.emit("send-location", { latitude, longitude });
+    },
+    (error) => {
+      console.log(error);
+    },
+    {
+      enableHighaccuracy: true,
+      timeout: 5000,
+      maximumAge: 0, // no caching
+    }
+  );
+}
+
+const map = L.map("map").setView([1, 5], 10);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution: "dhruv",
+}).addTo(map);
+
+const markers = {};
+
+socket.on("receive-location", (data) => {
+  const { id, latitude, longitude } = data;
+  map.setView([latitude, longitude], 16);
+  if (markers[id]) {
+    markers[id].setLatLng([latitude, longitude]);
+  } else {
+    markers[id] = L.marker([latitude, longitude]).addTo(map);
+  }
+});
+
+socket.on("user-disconnected", (id) => {
+  if (markers[id]) {
+    map.removeLayer(markers[id]);
+    delete markers[id];
+  }
+});
